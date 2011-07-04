e3f237c40299a61e776bc0107fc4f38ed206360a
Thomas Niederberger <thomas@niederberger.ch>
2011-07-04 18:34:16 +0200

    test

---
+++ src/ch/niederb/cct/VApplication.java
@@ -0,0 +1,18 @@
+package ch.niederb.cct;
+
+import android.app.Application;
+import android.content.Context;
+
+public class VApplication extends Application {
+
+    private static Context context;
+
+    public void onCreate(){
+    	VApplication.context = getApplicationContext();
+    	Speaker.init(context);
+    }
+
+    public static Context getAppContext() {
+    	return context;
+    }
+}
